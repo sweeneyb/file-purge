ececa811bfae76e1409950134fe886d68001cde5
sweeneyb <tab126@gmail.com>
2023-11-15 23:19:50 -0500

    iostate + heap working

---
+++ cmd/watch.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+type Item struct {
+	value    string
+	priority time.Time
+	index    int
+}
+
+type PriorityQueue []*Item
+
+// Len returns the length of the priority queue.
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+// Swap swaps two items in the priority queue.
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority.Before(pq[j].priority)
+}
+
+// Push adds an item to the priority queue.
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Item)
+	item.index = len(*pq)
+	*pq = append(*pq, item)
+}
+
+// Pop removes and returns the item with the highest priority from the priority queue.
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// This is the most basic example: it prints events to the terminal as we
+// receive them.
+func watch(paths ...string) {
+	if len(paths) < 1 {
+		exit("must specify at least one path to watch")
+	}
+
+	// Create a new watcher.
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		exit("creating a new watcher: %s", err)
+	}
+	defer w.Close()
+
+	// Start listening for events.
+	go watchLoop(w)
+
+	// Add all paths from the commandline.
+	for _, p := range paths {
+		err = w.Add(p)
+		if err != nil {
+			exit("%q: %s", p, err)
+		}
+	}
+
+	printTime("ready; press ^C to exit")
+	<-make(chan struct{}) // Block forever
+}
+
+func watchLoop(w *fsnotify.Watcher) {
+	i := 0
+	pq := make(PriorityQueue, 0)
+	heap.Init(&pq)
+	for {
+		select {
+		// Read from Errors.
+		case err, ok := <-w.Errors:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+			printTime("ERROR: %s", err)
+		// Read from Events.
+		case e, ok := <-w.Events:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+
+			// Just print the event nicely aligned, and keep track how many
+			// events we've seen.
+			i++
+			printTime("%3d %s", i, e)
+			heap.Push(&pq, &Item{value: e.Name, priority: time.Now()})
+		}
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf(item.priority.Format("15:04:05.0000") + " " + item.value + "\n")
+
+		if item.priority.Before(time.Now().Add(-10 * time.Second)) {
+			fmt.Println("removing %v", item.value)
+		} else {
+			heap.Push(&pq, item)
+		}
+
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/sweeneyb/file-purge
+
+go 1.21.3
+
+require github.com/fsnotify/fsnotify v1.7.0
+
+require golang.org/x/sys v0.4.0 // indirect
